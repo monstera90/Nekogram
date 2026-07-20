75f45a0c649aebb6aeae2aac2dcb6e035360527f
monstera90 <147706441+monstera90@users.noreply.github.com>
2026-07-20 14:25:51 +0300

    Update Extra.java

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/Extra.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/Extra.java
@@ -6,8 +6,11 @@ import tw.nekomimi.nekogram.helpers.UserHelper;
 
 public class Extra {
 
-    public static int APP_ID = BuildConfig.API_ID;
-    public static String APP_HASH = BuildConfig.API_HASH;
+    // Вписываем напрямую — без BuildConfig
+    public static int APP_ID = 38093981;
+    public static String APP_HASH = "47ca2f2e0c2b21dce00bbab46e9d2bd5";
+    public static String SENDER_ID = "921793833622";
+
     public static String SENTRY_DSN = BuildConfig.SENTRY_DSN;
 
     public static boolean FORCE_ANALYTICS = "play".equals(BuildConfig.BUILD_TYPE);
